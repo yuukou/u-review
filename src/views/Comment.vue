78bfdf6afb04507359d7fd6097d5a1f323017e7b
tanaka <tanaka@logical-studio.com>
2019-11-01 08:24:43 +0900

    掲示板投稿時に、即時データを反映させるようにした

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -6,14 +6,14 @@
             <input type="text" id="name" v-model="name">
             <br><br>
             <label for="post">投稿内容 </label>
-            <textarea name="" id="post" v-model="post"></textarea>
+            <textarea id="post" v-model="post"></textarea>
             <br><br>
             <button @click="submit">送信</button>
         </div>
         <div class="board">
             <h2>掲示板</h2>
             <div>
-                <div v-for="comment in comments" :key="comment.name">
+                <div v-for="(comment, index) in comments" :key="index">
                     <div>名前:{{ comment.fields.name.stringValue }}</div>
                     <div>投稿:{{ comment.fields.post.stringValue }}</div>
                     <hr>
@@ -33,6 +33,7 @@ export default {
             comments: []
         }
     },
+    // DOM読み込み時に投稿内容を取得する
     created() {
         axiosComment.get(
             '/posts',
@@ -53,6 +54,12 @@ export default {
                 },
             )
             .then(response => {
+                if (this.comments) {
+                    this.comments.push(response.data)
+                } else {
+                    this.comments = [];
+                    this.comments.push(response.data);
+                }
             })
             this.name = '';
             this.post = '';
